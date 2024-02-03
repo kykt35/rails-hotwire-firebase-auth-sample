b58eb2056783c4f2d3cb351f73d87623ad690393
kykt35 <kykt35@gmail.com>
2024-02-03 18:34:37 +0900

    Add firebase-admin-sdk gem and create client lib

---
+++ lib/firebase_admin/client.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'firebase-admin-sdk'
+
+module FirebaseAdmin
+  class Client
+    class << self
+      # ひとまず直接ファイルを指定しているが、環境変数にするなどしても良い
+      FIREBSE_SERVICE_ACCOUNT_FILE = Rails.root.join('service_account.json')
+
+      # Verify id token
+      #
+      # @param id_token [String] Firebase id token
+      # @return [hash] decored token
+      def verify_id_token!(id_token)
+        app.auth.verify_id_token(id_token)
+      end
+
+      private
+
+      def app
+        @app ||= Firebase::Admin::App.new(
+          credentials: Firebase::Admin::Credentials.from_file(FIREBSE_SERVICE_ACCOUNT_FILE)
+        )
+      end
+    end
+  end
+end
