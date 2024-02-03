bd2a2f69bc02be8064dbe30a2600ddb61db3f451
kykt35 <kykt35@gmail.com>
2024-02-03 18:35:03 +0900

    Add authenticate method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  def authenticate
+    @require_auth = true
+
+    id_token = request.headers['Authorization']&.split&.last
+
+    if request.method_symbol == :get && id_token.nil?
+      render 'shared/check_auth', status: 200
+      return
+    end
+
+    decorded_token = FirebaseAdmin::Client.verify_id_token!(id_token)
+    @uid = decorded_token['user_id']
+  end
 end
